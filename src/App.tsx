d2b75f8fa66247710a068777ec031868e506bde4
jakeychen <jakeychen@tencent.com>
2025-06-17 19:26:01 +0800

    feat: 新增展示背景图片

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import { getBingImage } from "./utils/utils";
 
 function App() {
   const [count, setCount] = useState(0);
-  // const [backgroundImage, setBackgroundImage] = useState("");
+  const [backgroundImage, setBackgroundImage] = useState("");
 
   const init = async () => {
     const imgUrl = await getBingImage();
     console.log(imgUrl);
-    // setBackgroundImage(imgUrl);
+    setBackgroundImage(imgUrl);
   };
 
   useEffect(() => {
@@ -19,25 +19,29 @@ function App() {
   }, []);
 
   return (
-    <div className="page-body">
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      className="back-page-body"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="page-content">
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            数量是 {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          数量是 {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <img />
     </div>
   );
 }
